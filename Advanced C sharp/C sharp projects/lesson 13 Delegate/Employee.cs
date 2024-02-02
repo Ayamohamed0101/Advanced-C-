929b54e1313ed2d3ec3b3fb0986735b72475a3f2
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-02-02 19:57:14 +0200

    Add files via upload

---
+++ Advanced C sharp/C sharp projects/lesson 13 Delegate/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace lesson_13_Delegate
+{
+  
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal TotalSales { get; set; }
+            public string Gender { get; set; }
+
+
+
+        }
+
+}
+
---
+++ Advanced C sharp/C sharp projects/lesson 13 Delegate/Employeec.cs
@@ -0,0 +1,10 @@
+﻿namespace lesson_13_Delegate
+{
+    public class Employeec
+    {
+        public string Gender { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal TotalSales { get; set; }
+    }
+}
